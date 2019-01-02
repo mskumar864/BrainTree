39c335b023593072f3ee9ce79dfce78b3bc425d8
suresh Kumar <sureshkumar.medasani@gmail.com>
2019-01-02 12:29:54 +0530

    BT APM native sandbox

--- app.js
+++ app.js
@@ -36,7 +36,6 @@ app.post("/checkout", function (req, res) {
     //    accessToken: "access_token$sandbox$55g8cq9ftqx5ysq5$1082e02169772c9637782fbd470d2aaa"
         accessToken : "access_token$production$96vrdvzcgbzj475g$012a9b891176afc4ea1aeba77bf53d93"
     });
-      console.log('nonce from the client ----------',nonceFromTheClient);
       // Use the payment method nonce here
       var nonceFromTheClient = req.body.nonce;
       console.log(nonceFromTheClient)
@@ -66,7 +65,7 @@ app.post("/checkout", function (req, res) {
     
     var gateway = braintree.connect({
       environment: braintree.Environment.Sandbox,
-     accessToken: "access_token$sandbox$cmsjrxqjrjzbcz2r$3ea9b37593fb87eccaa70d92ddf6babf"
+        accessToken: "access_token$sandbox$cmsjrxqjrjzbcz2r$3ea9b37593fb87eccaa70d92ddf6babf"
       //accessToken : "access_token$production$96vrdvzcgbzj475g$012a9b891176afc4ea1aeba77bf53d93"
   });
   
@@ -106,5 +105,55 @@ app.post("/checkout", function (req, res) {
         });
   
     });
+
+    app.get("/client_token_sb_bt", function (req, res) {
+      console.log('client_token');
+      
+      var gateway = braintree.connect({
+        environment: braintree.Environment.Sandbox,
+         // accessToken: "access_token$sandbox$cmsjrxqjrjzbcz2r$3ea9b37593fb87eccaa70d92ddf6babf"
+         merchantId: "wmc99tkkch665f7h",
+         publicKey: "k92zxzj52fsfj27m",
+         privateKey: "7453bd31fb6b8dc4ea894e4d820950df"
+            });
+    
+      gateway.clientToken.generate({}, function (err, response) {
+        res.send(response.clientToken);
+      });
+    });
+    
+    app.post("/checkout_sb_bt", function (req, res) {
+    
+      console.log("final call -----------------------------")
+        var gateway = braintree.connect({
+            environment: braintree.Environment.Sandbox,
+        //    accessToken: "access_token$sandbox$55g8cq9ftqx5ysq5$1082e02169772c9637782fbd470d2aaa"
+        merchantId: "wmc99tkkch665f7h",
+        publicKey: "k92zxzj52fsfj27m",
+        privateKey: "7453bd31fb6b8dc4ea894e4d820950df"
+        });
+          console.log('nonce from the client ----------',nonceFromTheClient);
+          // Use the payment method nonce here
+          var nonceFromTheClient = req.body.nonce;
+          console.log(nonceFromTheClient)
+          // Create a new transaction for $10
+          var newTransaction = gateway.transaction.sale({
+            amount: req.body.amount,
+            paymentMethodNonce: nonceFromTheClient,
+            merchantAccountId: "EUR",
+            options: {
+              // This option requests the funds from the transaction
+              // once it has been authorized successfully
+              submitForSettlement: true
+            }
+          }, function(error, result) {
+              if (result) {
+                res.send(result);
+              } else {
+                res.status(500).send(error);
+              }
+          });
+    
+      });
 
 module.exports = app;
